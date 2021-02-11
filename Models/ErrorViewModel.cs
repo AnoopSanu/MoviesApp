579f70253537f4eaa496e0b9e185f14b2d64a42e
ANOOP <manjushanmughan123@gmail.com>
2021-02-11 23:57:19 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyFavouriteMovies.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
